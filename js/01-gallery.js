32c69e29d1a5cbe47308309b954448a63aa657e6
analepekha <maiboroda0103@gmail.com>
2022-07-17 17:41:29 +0300

    done tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const markUpGallery = document.querySelector('.gallery')
+
+
+const makeGallery = (galleryItems) => {
+    return galleryItems.map(({ preview, original, description }) => {
+       return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    }).join("")
+}
+
+markUpGallery.insertAdjacentHTML('beforeend', makeGallery(galleryItems))
+
+const clickOnImage = (event) => {
+    event.preventDefault()
+    if (event.target.classList.contains('gallery__link')) {
+    return
+    }
+    const backdroup = basicLightbox.create(`
+    <img src="${event.target.dataset.source}">`)
+   
+    backdroup.show(() => window.addEventListener('keydown', onKeyPress))
+    
+    const onKeyPress = (event) => {
+    if (event.key === 'Escape') {
+        backdroup.close(() => window.removeEventListener('keydown', onKeyPress));
+        return;
+    }
+    return;
+};
+}    
+
+markUpGallery.addEventListener('click', clickOnImage)
 
 console.log(galleryItems);
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const markUpGallery = document.querySelector('.gallery')
+
+
+const makeGallery = (galleryItems) => {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" 
+        href="${original}">
+        <img class="gallery__image" 
+        src="${preview}" 
+        alt="${description}" />
+        </a>`
+    }).join("")
+}
+markUpGallery.insertAdjacentHTML('beforeend', makeGallery(galleryItems))
+
+  const gallerySimpleLightbox = new SimpleLightbox(".gallery .gallery__item", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+const descriptionOfImg = (event) => {
+    event.preventDefault()
+    if (event.target.classList.contains('gallery__image')) {
+        return gallerySimpleLightbox
+    }
+}
 
 console.log(galleryItems);
